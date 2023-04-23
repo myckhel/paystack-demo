afd2d5d152c20766085ed520646fd7a9851baf25
Myckhel <myckhel1@hotmail.com>
2023-04-23 14:35:07 +0100

    fixdd demo link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,7 +99,7 @@
                             </svg>
                         </a>
 
-                        <a href="https://github.com/myckhel/laravel-paystack" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <a href="https://github.com/myckhel/paystack-demo" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                             <div>
                                 <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="24" height="24" stroke-width="1.5" viewBox="0 0 24 24" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
